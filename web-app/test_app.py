ce4cf971d6d238c47f1fd3b8b2422c536b5935a6
Steve <96410601+Hyteve@users.noreply.github.com>
2023-12-04 01:43:54 -0500

    Update test_app.py

--- web-app/test_app.py
+++ web-app/test_app.py
@@ -29,69 +29,27 @@ def test_trigger_processing(mock_get, client):
     assert response.status_code == 200
     assert response.json == {"status": "processed"}
 
-# ... other tests ...
 
+@patch('pymongo.collection.Collection.find')
+def test_show_processed_image(mock_find, client):
+    """Test the show processed image route."""
+    # Mock the MongoDB find method
+    mock_find.return_value = [{
+        '_id': 1,
+        'processed_image': b'fake_image_data'
+    }]
+    response = client.get("/show-processed-image")
+    assert response.status_code == 200
+    assert 'text/html' in response.content_type
+    # You can add more assertions here to check for specific content in the response
 
-# Additional tests for '/show-processed-image' and '/visualize_data' can be written in a similar fashion.
+@patch('pymongo.collection.Collection.find_one')
+def test_visualize_data(mock_find_one, client):
+    """Test the visualize data route."""
+    # Mock the MongoDB find_one method
+    mock_find_one.return_value = {'some': 'data'}
+    response = client.get("/visualize_data")
+    assert response.status_code == 200
+    assert 'text/html' in response.content_type
+    # Similar to above, add more assertions as needed
 
-
-# import base64
-
-# import pytest
-
-# from app import app
-
-
-# @pytest.fixture
-# def client():
-#     app.config["TESTING"] = True
-#     with app.test_client() as client:
-#         yield client
-
-
-# def test_index_route(client):
-#     """Test the index route."""
-
-#     # Simulate a GET request to the '/' route
-#     response = client.get("/")
-
-#     # Check if the response is 200 OK
-#     # We can do more checks here if we want
-#     assert response.status_code == 200
-#     assert b"Data from MongoDB" in response.data
-
-
-# ## new tests but not passed
-# def test_index_post_route(client):
-#     """Test POST request to the index route."""
-#     sample_image_data = 'data:image/png;base64,' + base64.b64encode(b'test image data').decode()
-#     response = client.post("/", json={'imageData': sample_image_data})
-#     assert response.status_code == 200
-#     assert response.json['status'] == 'success'
-
-# def test_trigger_processing_route(client, mocker):
-#     """Test the trigger-processing route."""
-#     mocker.patch('requests.get', return_value=MockResponse())
-#     response = client.post('/trigger-processing')
-#     assert response.status_code == 200
-#     # Add more assertions based on expected response
-
-# def test_show_processed_image_route(client):
-#     """Test the show-processed-image route."""
-#     response = client.get('/show-processed-image')
-#     assert response.status_code == 200
-#     assert b'<img src=' in response.data
-
-# def test_visualize_data_route(client):
-#     """Test the visualize_data route."""
-#     response = client.get('/visualize_data')
-#     assert response.status_code == 200
-#     # Add more assertions based on expected content
-
-# class MockResponse:
-#     """Mock response for external service calls."""
-#     def json(self):
-#         return {'status': 'success', 'message': 'Processed image'}
-
-# if __name__ == "__main__":
-#     pytest.main()
